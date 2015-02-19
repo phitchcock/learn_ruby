e447c83fa7775e06ec829fe5e47ef9005fb3251b
phitchcock <phitchcock@me.com>
2015-02-19 13:51:09 -0800

    stuck on titleize

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -7,5 +7,17 @@ def shout(str)
 end
 
 def repeat(x, y = 2)
-  y.times.map { x + ' ' }.join
+  y.times.map { x + ' ' }.join.strip
+end
+
+def start_of_word(word, x = 1)
+  word[0,x]
+end
+
+def first_word(word)
+  word.slice(0..((word.index(' ')) - 1))
+end
+
+def titleize(word)
+  word.split(' ').map{|w1| w1.capitalize}.join(' ')
 end
